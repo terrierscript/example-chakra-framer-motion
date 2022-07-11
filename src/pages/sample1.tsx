952b53fa48cdfa7a9cded96acbc5853d22fb2b48
S. Suzuki <terrierscript@gmail.com>
2022-07-11 23:21:20 +0900

    add

--- src/pages/sample1.tsx
+++ src/pages/sample1.tsx
@@ -1,43 +1,57 @@
 
-import { Box, chakra, HStack, Stack } from '@chakra-ui/react'
-import { motion, isValidMotionProp } from 'framer-motion'
+import { Box, BoxProps, chakra, HStack, Stack } from '@chakra-ui/react'
+import { motion, isValidMotionProp, Transition } from 'framer-motion'
 import { FC, PropsWithChildren } from 'react'
 
 const ChakraBox = chakra(motion.div, {
   shouldForwardProp: isValidMotionProp,
 })
 
-const Rotate: FC<PropsWithChildren<{}>> = ({ children }) => {
+const Rotate: FC<PropsWithChildren<{
+  transition?: Transition
+}>> = ({ children, transition }) => {
+  const defaultTransition: Transition = {
+    duration: 3,
+    repeat: Infinity,
+  }
   return <motion.div
     animate={{
       scale: [0.1, 1, 0.1],
       rotate: [0, 360],
     }}
     transition={{
-      duration: 3,
-      repeat: Infinity
+      ...defaultTransition,
+      ...transition,
     }}
   >
     {children}
   </motion.div>
 }
 
+const RotateBox = (props: BoxProps) => {
+  return <Box
+    p={2}
+    bg="red.200"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    {...props}
+  />
+}
 const Page = () => {
   return <Box p={10}>
     <HStack>
       <Box w={100} h={100}>
-        <Rotate>
-          <Box
-            w={100} h={100}
-            p={2}
-            bg="red.200"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Box>
-            </Box>
-          </Box>
+        <Rotate><RotateBox w={100} h={100} /></Rotate>
+      </Box>
+      <Box w={100} h={100}>
+        <Rotate transition={{ delay: 0.5 }}>
+          <RotateBox w={100} h={100} />
+        </Rotate>
+      </Box>
+      <Box w={100} h={100}>
+        <Rotate transition={{ delay: 1 }}>
+          <RotateBox w={100} h={100} />
         </Rotate>
       </Box>
     </HStack>
